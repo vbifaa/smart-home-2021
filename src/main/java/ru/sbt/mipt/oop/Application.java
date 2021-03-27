5fc713a6d208752f7bb0134a801e88ee4b8cea86
Якубенко Михаил <yakubenko.mish@yandex.by>
2021-03-27 19:20:48 +0300

    Add Spring loC

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,40 +1,16 @@
 package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import ru.sbt.mipt.oop.adapters.EventHandlerAdapter;
-import ru.sbt.mipt.oop.decorators.SignalingDecorator;
-import ru.sbt.mipt.oop.event.processors.*;
-import ru.sbt.mipt.oop.file.readers.FileContentReaderImpl;
-import ru.sbt.mipt.oop.file.readers.SmartHomeJsonReader;
-import ru.sbt.mipt.oop.file.readers.SmartHomeReader;
-import ru.sbt.mipt.oop.signaling.Signaling;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import ru.sbt.mipt.oop.configurations.DefaultConfiguration;
 
 
 public class Application {
 
     public static void main(String... args) {
-        SmartHomeReader smartHomeReader = new SmartHomeJsonReader(new FileContentReaderImpl());
-        SmartHome smartHome = smartHomeReader.read("smart-home-1.js");
-
-
-        Signaling signaling = new Signaling();
-        EventProcessor handler = new CompositeHandler(
-                Arrays.asList(
-                        new LightEventProcessor(smartHome),
-                        new DoorEventProcessor(smartHome),
-                        new HallDoorEventProcessor(smartHome),
-                        new SignalingEventProcessor(signaling)
-                )
-        );
-        SensorEventsManager manager = new SensorEventsManager();
-        EventHandlerAdapter adapter = new EventHandlerAdapter(
-                new SignalingDecorator(
-                    handler, signaling, new SmsSenderImpl()
-                )
-        );
-        manager.registerEventHandler(adapter);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(DefaultConfiguration.class);
+        SensorEventsManager manager = context.getBean(SensorEventsManager.class);
         manager.start();
     }
 
---
+++ src/main/java/ru/sbt/mipt/oop/configurations/DefaultConfiguration.java
@@ -0,0 +1,96 @@
+package ru.sbt.mipt.oop.configurations;
+
+import com.coolcompany.smarthome.events.SensorEventsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.sbt.mipt.oop.SmartHome;
+import ru.sbt.mipt.oop.SmsSender;
+import ru.sbt.mipt.oop.SmsSenderImpl;
+import ru.sbt.mipt.oop.adapters.EventHandlerAdapter;
+import ru.sbt.mipt.oop.decorators.SignalingDecorator;
+import ru.sbt.mipt.oop.event.processors.*;
+import ru.sbt.mipt.oop.file.readers.FileContentReader;
+import ru.sbt.mipt.oop.file.readers.FileContentReaderImpl;
+import ru.sbt.mipt.oop.file.readers.SmartHomeJsonReader;
+import ru.sbt.mipt.oop.file.readers.SmartHomeReader;
+import ru.sbt.mipt.oop.signaling.Signaling;
+
+import java.util.Arrays;
+
+@Configuration
+public class DefaultConfiguration {
+
+    @Bean
+    SensorEventsManager defaultSensorEventsManager() {
+        SensorEventsManager res = new SensorEventsManager();
+        res.registerEventHandler(adapter());
+        return res;
+    }
+
+    @Bean
+    EventHandlerAdapter adapter() {
+        return new EventHandlerAdapter(eventProcessorDecorator());
+    }
+
+    @Bean
+    EventProcessor eventProcessorDecorator() {
+        return new SignalingDecorator(handler(), signaling(), smsSender());
+    }
+
+    @Bean
+    SmsSender smsSender() {
+        return new SmsSenderImpl();
+    }
+
+    @Bean
+    EventProcessor handler() {
+        return new CompositeHandler(
+                Arrays.asList(
+                        doorEventProcessor(),
+                        lightEventProcessor(),
+                        hallDoorEventProcessor(),
+                        signalingEventProcessor()
+                )
+        );
+    }
+
+    @Bean
+    EventProcessor doorEventProcessor() {
+        return new DoorEventProcessor(smartHome());
+    }
+
+    @Bean
+    EventProcessor lightEventProcessor() {
+        return new LightEventProcessor(smartHome());
+    }
+
+    @Bean
+    EventProcessor hallDoorEventProcessor() {
+        return new HallDoorEventProcessor(smartHome());
+    }
+
+    @Bean
+    EventProcessor signalingEventProcessor() {
+        return new SignalingEventProcessor(signaling());
+    }
+
+    @Bean
+    Signaling signaling() {
+        return new Signaling();
+    }
+
+    @Bean
+    SmartHome smartHome() {
+        return smartHomeReader().read("smart-home-1.js");
+    }
+
+    @Bean
+    SmartHomeReader smartHomeReader() {
+        return new SmartHomeJsonReader(fileContentReader());
+    }
+
+    @Bean
+    FileContentReader fileContentReader() {
+        return new FileContentReaderImpl();
+    }
+}

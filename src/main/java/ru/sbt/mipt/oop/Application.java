510b026afb70d187e5b5371a8d0f225f4ea67b0b
Якубенко Михаил <yakubenko.mish@yandex.by>
2021-04-14 00:08:11 +0300

    Add RemoteControlConfiguration

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -3,14 +3,14 @@ package ru.sbt.mipt.oop;
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import ru.sbt.mipt.oop.configurations.LibraryConfiguration;
+import ru.sbt.mipt.oop.configurations.RemoteControlConfiguration;
 
 
 public class Application {
 
     public static void main(String... args) {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(
-                LibraryConfiguration.class
+                RemoteControlConfiguration.class
         );
         SensorEventsManager manager = context.getBean(SensorEventsManager.class);
         manager.start();
--- src/main/java/ru/sbt/mipt/oop/configurations/RemoteControlConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/configurations/RemoteControlConfiguration.java
@@ -1,5 +1,74 @@
 package ru.sbt.mipt.oop.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import rc.RemoteControl;
+import rc.RemoteControlRegistry;
+import ru.sbt.mipt.oop.event.processors.EventProcessor;
+import ru.sbt.mipt.oop.remote.controllers.DefaultRemoteController;
+import ru.sbt.mipt.oop.remote.controllers.commands.*;
+
+import java.util.HashMap;
+
+@Configuration
+@ComponentScan
+@Import(SignalingDecoratorConfiguration.class)
 public class RemoteControlConfiguration {
 
+    @Autowired
+    @Qualifier("signalingDecorator")
+    private EventProcessor processor;
+
+    @Bean
+    RemoteControlRegistry remoteControlRegistry() {
+        RemoteControlRegistry rc = new RemoteControlRegistry();
+        rc.registerRemoteControl(remoteControl(), "1");
+        return rc;
+    }
+
+    @Bean
+    RemoteControl remoteControl() {
+        return new DefaultRemoteController(new HashMap<String, Command>(){{
+            put("A", activateSignaling());
+            put("B", closeHallDoor());
+            put("C", setAlarmSignaling());
+            put("D", turnOffAllLight());
+            put("1", turnOnAllLight());
+            put("2", turnOnLightInHall());
+        }});
+    }
+
+    @Bean
+    Command activateSignaling() {
+        return new ActivateSignaling(processor,"ummu");
+    }
+
+    @Bean
+    Command closeHallDoor() {
+        return new CloseHallDoor(processor, "1");
+    }
+
+    @Bean
+    Command setAlarmSignaling() {
+        return new SetAlarmSignaling(processor);
+    }
+
+    @Bean
+    Command turnOffAllLight() {
+        return new TurnOnAllLight(processor);
+    }
+
+    @Bean
+    Command turnOnAllLight() {
+        return new TurnOnAllLight(processor);
+    }
+
+    @Bean
+    Command turnOnLightInHall() {
+        return new TurnOnLightInHall(processor);
+    }
 }
